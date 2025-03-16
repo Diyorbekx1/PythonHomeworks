ba4b35d7b6ffb5ae0ff9b76949e2ebb229b4d46a
Diyorbekx1 <thenarimonov@gmail.com>
2025-03-16 17:55:20 +0500

    Create homework-3.py

---
+++ lesson-3/homework/homework-3.py
@@ -0,0 +1,80 @@
+1)
+fruits = ["Apple", "Banana", "Cherry", "Orange", "Stroberry"]
+print("Third fruit:", fruits[2])
+
+2)
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_list = list1 + list2
+print("Concatenated List:", concatenated_list)
+
+3)
+
+numbers = [10, 20, 30, 40, 50]
+middle_index = len(numbers) // 2
+extracted_elements = [numbers[0], numbers[middle_index], numbers[-1]]
+print("Extracted Elements:", extracted_elements)
+4)
+favorite_movies = ["Fight club", "Truman show", "Interstellar", "Spideman", "Limitless"]
+movie_tuple = tuple(favorite_movies)
+print("Movies Tuple:", movie_tuple)
+
+6)
+numbers = [1, 2, 3, 4, 5]
+duplicated_list = numbers * 2
+print("Duplicated List:", duplicated_list)
+
+7)
+numbers = [10, 20, 30, 40, 50]
+numbers[0], numbers[-1] = numbers[-1], numbers[0]
+print("List after swapping first and last elements:", numbers)
+8)
+numbers_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+sliced_tuple = numbers_tuple[3:8]
+print("Sliced Tuple:", sliced_tuple)
+
+9)
+colors = ["red", "blue", "green", "blue", "yellow", "blue"]
+blue_count = colors.count("blue")
+print("Occurrences of 'blue':", blue_count)
+
+10)
+animals = ("dog", "cat", "lion", "elephant", "tiger")
+lion_index = animals.index("lion")
+print("Index of 'lion':", lion_index)
+
+11)
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+merged_tuple = tuple1 + tuple2
+print("Merged Tuple:", merged_tuple)
+
+12)
+my_list = [10, 20, 30, 40]
+my_tuple = (1, 2, 3, 4, 5)
+list_length = len(my_list)
+tuple_length = len(my_tuple)
+print("Length of List:", list_length)
+print("Length of Tuple:", tuple_length)
+
+13)
+my_tuple = (1, 2, 3, 4, 5)
+converted_list = list(my_tuple)
+print("Converted List:", converted_list)
+
+14)
+numbers_tuple = (10, 20, 30, 40, 50)
+max_value = max(numbers_tuple)
+min_value = min(numbers_tuple)
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
+
+15)
+words_tuple = ("apple", "banana", "cherry", "Orange", "Stroberry")
+reversed_tuple = words_tuple[::-1]
+print("Reversed Tuple:", reversed_tuple)
+
+
+
+
+
