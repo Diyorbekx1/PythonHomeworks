a842e80d8a933bae05aa85f7c8f695b0a19a22a2
Diyorbekx1 <thenarimonov@gmail.com>
2025-03-16 14:54:32 +0500

    Create homework-2.py

---
+++ lesson-2/homework/homework-2.py
@@ -0,0 +1,36 @@
+1)
+age=17
+name="Diyorbek"
+print(f"{name},his age is{age}")
+
+2)
+txt = 'LMaasleitbtui'
+car_name = txt[1:3]+txt[5:7]+txt[9]+txt[11] 
+print("Car Name: ", car_name)
+
+3)
+txt = 'MsaatmiazD'
+car_name = txt[0] + txt[3:5]+txt[6]+txt[8]
+print("Car Name: ", car_name)
+
+4)
+txt = "I'am John. I am from London"
+residence_area = txt.split("from ")[1]
+print("Residence Area: ", residence_area)
+
+5)
+user_input = input("hello")
+reversed_string = user_input[::-1]
+print("Reversed string:", reversed_string)
+6)
+user_input = input("Hello world")
+vowels = "aeiouAEIOU"
+count = sum(1 for char in user_input if char in vowels)
+print(f"Number of vowels: {count}")
+9)
+    email = input("thenarimonov@gmail.com ")
+    domain = email.split('@')[1]
+    print(f"Email domain: {domain}")
+10)
+
+
