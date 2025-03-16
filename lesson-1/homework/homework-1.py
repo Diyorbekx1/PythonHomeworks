874e7a65fac8c97f96d4c6c6d298d66d8f2a8552
Diyorbekx1 <thenarimonov@gmail.com>
2025-03-16 12:12:44 +0500

    Create homework-1.py

---
+++ lesson-1/homework/homework-1.py
@@ -0,0 +1,28 @@
+# 1
+side=5
+perimeter = 4 * side
+area = side * side
+print( perimeter, area)
+
+
+# 2  
+diameter = 10
+circumference = (diameter)
+print(f"Circle - Circumference: {circumference}")
+
+
+# 3
+
+a, b = 4, 6
+mean = (a + b) / 2
+print(f"Mean of {a} and {b}: {mean}")
+
+#4
+a, b = 4, 6
+Z
+sum_ab = a + b
+product_ab = a * b
+a_squared = a ** 2
+b_squared = b ** 2
+print(f"Sum: {sum_ab}, Product: {product_ab}, Squares: {a_squared}, {b_squared}")
+
